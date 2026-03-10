fe785cb0a8eae01efd22d20efe5fb5f60acd9ea0
Ashen1919 <2303S0834@saegis.ac.lk>
2026-03-10 09:11:02 +0530

    Initial commit

---
+++ src/test/java/org/dev_projects/blog_api/BlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev_projects.blog_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
